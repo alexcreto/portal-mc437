071443acdfcb5ea5c7d91b3a85018ab3c1515c00
alexcreto <alexcreto@gmail.com>
2012-11-08 17:43:36 -0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PortalMc437::Application.config.secret_token = 'ebe6d8daf3f857ea222bbe0332cd1df67cd50cbb659068ef5a87e1a66e19c05c9c32a4c64ea43b7a241b678b49375c9b12730b64bd32faa0b11eca0176dcbb4b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+PortalMc437::Application.config.session_store :cookie_store, :key => '_portal-mc437_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# PortalMc437::Application.config.session_store :active_record_store
