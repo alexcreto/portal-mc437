546bfe98e845efa2afb501a77d70f4c8dad85235
alexcreto <alexcreto@gmail.com>
2012-11-09 01:17:48 -0200

    products

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,12 @@ class HomeController < ApplicationController
   end
 
   def index
-    @rest = Home.get_json
+    @prod1 = Nestful.json_get "http://g1:g1@mc437-g8-estoque-v2.webbyapp.com/products/currentInfo/1010.json"
+    @prod2 = Nestful.json_get "http://g1:g1@mc437-g8-estoque-v2.webbyapp.com/products/currentInfo/2222.json"
+    @prod3 = Nestful.json_get "http://g1:g1@mc437-g8-estoque-v2.webbyapp.com/products/currentInfo/3030.json"
+    @prod4 = Nestful.json_get "http://g1:g1@mc437-g8-estoque-v2.webbyapp.com/products/currentInfo/3034.json"
+    @prod5 = Nestful.json_get "http://g1:g1@mc437-g8-estoque-v2.webbyapp.com/products/currentInfo/3234.json"
+    @prod6 = Nestful.json_get "http://g1:g1@mc437-g8-estoque-v2.webbyapp.com/products/currentInfo/3333.json"
   end
 
   def products
--- app/models/home.rb
+++ app/models/home.rb
@@ -1,10 +1,4 @@
 class Home < ActiveRecord::Base
   # attr_accessible :title, :body
 
-  def self.get_json
-    code = 3333
-    uri = "http://g1:g1@mc437-g8-estoque-v2.webbyapp.com/products/currentInfo/" + code.to_s + ".json"
-    rest = Nestful.json_get uri
-  end
-
 end
